145e372d3bfffa1ac789e37e72ce412702cca108
Nabeelah <noblenab13@gmail.com>
2020-02-06 18:32:22 +0100

    Connect to postgres

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,45 @@
+require('dotenv').config();
 module.exports = {
   development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./database/save_the_animals.db3"
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./database/migrations"
-    },
-    seeds: {
-      directory: "./database/seeds"
-    }
-  },
-  testing: {
-    client: 'sqlite3',
-    connection: {
-      filename: './database/save_the_animals_test.db3',
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
     },
-    seeds: {
-      directory: './database/seeds',
-    },
+    seeds: { directory: './database/seeds' }
+    // client: 'pg',
+    // connection: {
+    //   filename: './database/save_the_animals.db3'
+    // },
+    // useNullAsDefault: true,
+    // migrations: {
+    //   directory: './database/migrations'
+    // },
+    // seeds: {
+    //   directory: './database/seeds'
+    // }
   },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/save_the_animals_test.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: { directory: './database/seeds' }
+  }
 };
