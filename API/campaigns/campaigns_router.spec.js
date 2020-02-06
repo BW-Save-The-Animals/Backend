7a9a7065b29d8f1d19d951f46c2448fad8296562
ola-dola <oredolaolamide@gmail.com>
2020-02-06 12:41:53 +0100

    test: campaigns get endpoints

--- API/campaigns/campaigns_router.spec.js
+++ API/campaigns/campaigns_router.spec.js
@@ -1,18 +1,51 @@
 const server = require("../../server");
 const request = require("supertest");
 
-describe("server.js module", () => {
-  describe("campaign endpoints", () => {
-    it("returns 200 http status code", async () => {
-      const login = await request(server)
-        .get("/api/campaigns")
-        .set("Accept-Language", "en")
-        .set("Cookie", [
-          "session_cookie=s%3APKY0fh0kcfC1hjKhfRFVpUpFvwgmEIGv.m5pzdEw%2FsbAQxf28gtIdDAdWVeV8G4kwzi1%2BFm00ALI"
-        ])
-        .then(res => {
-          expect(res.status).toBe(200);
-        });
-    });
+describe("campaign endpoints", () => {
+  it("should have the right environment", () => {
+    expect(process.env.DB_ENV).toBe("testing");
   });
+
+  let cookie = "";
+
+  it("should set cookie", done => {
+    request(server)
+      .post("/api/auth/login")
+      .send({
+        email: "world@zoo.com",
+        password: "pass"
+      })
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json")
+      .then(response => {
+        cookie = response.header["set-cookie"][0];
+        expect(cookie).toContain("session_cookie");
+        done();
+      });
+  });
+
+  it("returns an array of campaigns", () => {
+    return request(server)
+      .get("/api/campaigns")
+      .set("Cookie", cookie)
+      .set("Content-Type", "application/json")
+      .then(res => {
+        expect(res.status).toBe(200);
+        expect(res.body).toBeInstanceOf(Array);
+        expect(res.body.length).not.toBe(0);
+      });
+  });
+
+  it("should get campaigns by id", () => {
+    return request(server)
+      .get("/api/campaigns/1")
+      .set("Cookie", cookie)
+      .set("Content-Type", "application/json")
+      .then(res => {
+        expect(res.status).toBe(200);
+        expect(res.body).toBeInstanceOf(Object);
+        expect(res.body).toHaveProperty("title");
+      });
+  });
+
 });
