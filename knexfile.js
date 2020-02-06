7b282bdbc53ca33b93d0fb1267f3e5704548df2a
Nabeelah <noblenab13@gmail.com>
2020-02-06 18:54:11 +0100

    Config development

--- knexfile.js
+++ knexfile.js
@@ -1,24 +1,17 @@
 require('dotenv').config();
 module.exports = {
   development: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
+    client: 'sqlite3',
+    connection: {
+      filename: './database/save_the_animals.db3'
+    },
     useNullAsDefault: true,
     migrations: {
-      directory: './database/migrations',
+      directory: './database/migrations'
     },
-    seeds: { directory: './database/seeds' }
-    // client: 'pg',
-    // connection: {
-    //   filename: './database/save_the_animals.db3'
-    // },
-    // useNullAsDefault: true,
-    // migrations: {
-    //   directory: './database/migrations'
-    // },
-    // seeds: {
-    //   directory: './database/seeds'
-    // }
+    seeds: {
+      directory: './database/seeds'
+    }
   },
   testing: {
     client: 'sqlite3',
@@ -38,7 +31,7 @@ module.exports = {
     connection: process.env.DATABASE_URL,
     useNullAsDefault: true,
     migrations: {
-      directory: './database/migrations',
+      directory: './database/migrations'
     },
     seeds: { directory: './database/seeds' }
   }
